a3e77adfeb9dc24be0593d253f89fd65fd2a02af
Aztechs2 <Aztechs2>
2018-03-27 19:41:41 -0400

    drive motors go to coast mode on autonomous mode 0

--- src/org/usfirst/frc157/FRC2018/commands/AutonMode0.java
+++ src/org/usfirst/frc157/FRC2018/commands/AutonMode0.java
@@ -2,18 +2,24 @@
 package org.usfirst.frc157.FRC2018.commands;
 
 import org.usfirst.frc157.FRC2018.Robot;
+import org.usfirst.frc157.FRC2018.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AutonMode0 extends Command
 {
-	public AutonMode0() {
-		
-	}
-	public void execute() {
-		System.out.println(Robot.drive.getAngle());
-	}
-	public boolean isFinished()
+    public AutonMode0() {
+    }
+    public void execute() {
+        System.out.println(Robot.drive.getAngle());
+        RobotMap.driveRight1.setNeutralMode(NeutralMode.Coast);
+        RobotMap.driveRight2.setNeutralMode(NeutralMode.Coast);
+        RobotMap.driveLeft1.setNeutralMode(NeutralMode.Coast);
+        RobotMap.driveLeft2.setNeutralMode(NeutralMode.Coast);
+    }
+    public boolean isFinished()
     {
         return false;
     }
